265bfa7b4ae9f450b37da567b4f5d654a9e6e3bb
Denver <gregoriomarkdenver01@gmail.com>
2025-11-24 23:18:15 +0800

    registerdriverchanges

--- src/main/java/com/carry_guide/carry_guide_admin/infrastructure/config/StaticResourceConfig.java
+++ src/main/java/com/carry_guide/carry_guide_admin/infrastructure/config/StaticResourceConfig.java
@@ -1,6 +1,5 @@
 package com.carry_guide.carry_guide_admin.infrastructure.config;
 
-import com.carry_guide.carry_guide_admin.service.FileUploadService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,22 +12,21 @@ import java.nio.file.Paths;
 
 @Configuration
 public class StaticResourceConfig implements WebMvcConfigurer {
-    private static final Logger log = LoggerFactory.getLogger(StaticResourceConfig.class);
 
-    @Value("${app.upload.folder}")
-    private String uploadFolder;
+    private static final Logger log = LoggerFactory.getLogger(StaticResourceConfig.class);
 
     @Value("${app.upload.folder.driver}")
     private String uploadFolderDriver;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        log.debug("register - addResourceHandlers");
-        registry.addResourceHandler("/upload/product/**")
-                .addResourceLocations("file:" + uploadFolder + "/");
 
-        // Driver images (photo, front ID, back ID)
-        registry.addResourceHandler("/upload/drivers/**")
-                .addResourceLocations("file:" + uploadFolderDriver + "/");
+        // Convert relative path (uploads/drivers) → absolute path
+        Path driverUploadPath = Paths.get(uploadFolderDriver).toAbsolutePath();
+
+        log.info("Driver Upload Path Mapped To: {}", driverUploadPath);
+
+        registry.addResourceHandler("/upload/driver/**")
+                .addResourceLocations("file:" + driverUploadPath + "/");
     }
 }
