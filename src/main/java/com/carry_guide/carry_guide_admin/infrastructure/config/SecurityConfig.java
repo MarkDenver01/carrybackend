f8cc0679fcf9f5abbee5713a6e349de51b13a805
Tani <gregorionathaniel01@gmail.com>
2025-11-29 18:52:38 +0800

    update security config (1)

--- src/main/java/com/carry_guide/carry_guide_admin/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/carry_guide/carry_guide_admin/infrastructure/config/SecurityConfig.java
@@ -160,11 +160,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/gmail/**").permitAll()
                         .requestMatchers("/api/xendit/**").permitAll()
                         .requestMatchers("/api/ewallet/**").permitAll()
-                        .requestMatchers(
-                                "/api/driver/location",
-                                "/api/driver/*/location",
-                                "/api/driver/*/location/stream"
-                        ).permitAll()
+                        .requestMatchers("/api/driver/**").permitAll()
                         .requestMatchers("/user/public/**").permitAll()
                         .requestMatchers("/upload/**").permitAll()
                         .requestMatchers("/upload/driver/**").permitAll()
